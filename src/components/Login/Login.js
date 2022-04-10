6b1ff0c5531c088bdad5d6a5dcd4aefa642e3e49
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-10 18:18:05 +0600

    login user done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
 import Logo from '../../images/Google.svg'
 
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 const Login = () => {
+    const [email, setEmail]=useState('')
+    const [password, setPassword]=useState('')
+    const navigate = useNavigate()
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailBlur= event=>{
+         setEmail(event.target.value)
+    }
+     const handlePasswordBlur=event=>{
+         setPassword(event.target.value)
+     } 
+
+     if(user){
+        navigate('/shop')
+     }
+     const handleUserSignIn=event=>{
+         event.preventDefault()
+
+         signInWithEmailAndPassword(email, password)
+     }
+
     return (
         <div className='form-container'>
            <div>
            <h2 className='form-title'>login</h2>
-              <form>
+              <form onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" required />
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id=""required/>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id=""required/>
                     </div>
                     <input className='submit-btn' type="submit" value="Login" />
+                    <p>{error?.message}</p>
+                    {
+                        loading && <p>loading...</p>
+                    }
               </form>
               <p>New to Ema-Jhon? <Link className='form-link' to="/signup">create an account</Link></p>
                <div className="form-horizontal">
