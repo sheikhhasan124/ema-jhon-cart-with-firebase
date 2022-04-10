fe3f851e5a97231a4b469c48fea8b3f8a77680c2
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-10 15:15:42 +0600

    login form design done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import About from './components/about/About';
 import Header from './components/Header/Header';
 import Inventory from './components/inventory/Inventory';
+import Login from './components/Login/Login';
 import Order from './components/order/Order';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/orders'element={<Order></Order>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
   
     </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>login</h2>
+        <div className='form-container'>
+           <div>
+           <h2 className='form-title'>login</h2>
+              <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='submit-btn' type="submit" value="Login" />
+              </form>
+              <p>New to Ema-Jhon? <Link className='form-link' to="/signup">create an account</Link></p>
+           </div>
         </div>
     );
 };
